45f10351530ec8a2e24f39aaad090162564f237c
ricardo <ricardo.berrospi@tecsup.edu.pe>
2017-10-03 23:31:20 -0500

    ricardo
    spring 1

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "krobawsky");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "gamarra");
+?>
